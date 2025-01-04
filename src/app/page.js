258aa76e375d46c11f6d9865e3cca8aba2f3bd94
preraana <koiralapreranaa@gmail.com>
2025-01-04 21:31:10 +0545

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-justify">
+        Creating a react application....
+      </h1>
+    </div>
+  );
+}
+
